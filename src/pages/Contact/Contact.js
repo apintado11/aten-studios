03bf2ce8eb7f9d18d55fe7a8a91265b24e569687
alex <pintado96@gmail.com>
2025-08-27 11:45:42 -0400

    contact info update.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -35,14 +35,14 @@ const Contact = () => {
                                     <svg className="w-5 h-5 text-custom-secondary mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                     </svg>
-                                    <span className="text-custom-primary">hello@atenstudios.com</span>
+                                    <span className="text-custom-primary">ap@atenstd.com</span>
                                 </div>
                                 <div className="flex items-center">
                                     <svg className="w-5 h-5 text-custom-secondary mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                                     </svg>
-                                    <span className="text-custom-primary">San Francisco, CA</span>
+                                    <span className="text-custom-primary">Lake Hopatcong, NJ</span>
                                 </div>
                             </div>
                         </div>
